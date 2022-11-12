4ed5651dbd6ff2146278a6fe66817c6eb0325bf0
UroojB <salahuddin.urooj@gmail.com>
2022-11-12 15:22:40 -0600

    1 test failing

--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -15,6 +15,13 @@ public class Skill extends AbstractEntity {
     @NotNull
     private List<Job> jobs = new ArrayList<>();
 
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
 
     public Skill() {
     }
